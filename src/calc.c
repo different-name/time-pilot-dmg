aea4cf8a40b75e1c0ef198636077d0f5a766f1b1
HarveyB02 <harvey.brett02@gmail.com>
2023-11-21 16:47:26 +1100

    Targeting Improvements

--- src/calc.c
+++ src/calc.c
@@ -1,97 +1,26 @@
 #include "calc.h"
 #include "main.h"
 
-// uint8_t angle_between_pos(Pos start, Pos target) {
-// 	if (start.x == target.x) {
-// 		return (start.y < target.y) * 8;
-// 	}
-// 	if (start.y == target.y) {
-// 		return (start.x > target.x) * 8 + 4;
-// 	}
-
-// 	int16_t x = FP_LIT((int16_t)target.x - (int16_t)start.x);
-// 	int16_t y = -FP_LIT((int16_t)target.y - (int16_t)start.y);
-
-// 	int16_t a = FP_DIV((int32_t)MIN(ABS(x), ABS(y)), (int32_t)MAX(ABS(x), ABS(y)));
-// 	int16_t s = FP_MUL(a, a);
-// 	int16_t r = FP_MUL(FP_MUL((FP_MUL((FP_MUL(-5, s) + 16), s) - 32), s), a) + a;
-
-// 	if (ABS(y) > ABS(x)) {
-// 		r = 157 - r;
-// 	}
-// 	if (x < 0) {
-// 		r = 314 - r;
-// 	}
-// 	if (y < 0) {
-// 		r = -r;
-// 	}
-
-// 	r = r - 157;
-
-// 	if (r > 0) {
-// 		r = 628 - r;
-// 	}
-// 	if (r < 0) {
-// 		r = -r;
-// 	}
-
-// 	return (((r + 5) / 10 * 25 / 10 + 5) / 10) % 16;
-// }
-
-uint8_t angle_between_pos(Pos start, Pos target) {
-	if (start.x == target.x) {
-		return (start.y < target.y) * 8;
+// Calculates the angle between two points as a 8 bit unsigned integer (0 to 15)
+uint8_t angle_between_pos(Pos p1, Pos p2) {
+	if (p1.x == p2.x) { // x coordinate is same
+		return (p1.y < p2.y) * 8; // Point up (0) or down (8)
 	}
-	if (start.y == target.y) {
-		return (start.x > target.x) * 8 + 4;
+	if (p1.y == p2.y) { // y coordinate is same
+		return (p1.x > p2.x) * 8 + 4; // Point right (4) or left (12)
 	}
 
-	if (target.x > start.x) {
-		if (target.y > start.y) {
-			uint8_t rise = target.y - start.y;
-			uint8_t run = target.x - start.x;
-			uint16_t slope = rise * 100 / run;
+	uint8_t left = p2.x < p1.x; // Target x is less than start x (target left of start)
+	uint8_t below = p2.y > p1.y; // Target y is greater than start y (target below start)
 
-			if (slope > 67) {
-				return (slope > 503) * 1 + (slope > 144) * 1 + 6;
-			} else {
-				return (slope > 10) * 1 + 4;
-			}
+	uint8_t angle = ((left << 1) | (left ^ below)) * 4; // Calculating quadrant as a number from 0 to 3, then multiplying by 4
 
-		} else {
-			uint8_t rise = start.y - target.y;
-			uint8_t run = target.x - start.x;
-			uint16_t slope = rise * 100 / run;
+	uint8_t dx = (p1.x > p2.x) ? p1.x - p2.x : p2.x - p1.x; // x distance between p1 and p2
+	uint8_t dy = (p1.y > p2.y) ? p1.y - p2.y : p2.y - p1.y; // y distance between p1 and p2
 
-			if (slope > 67) {
-				return (slope <= 503) * 1 + (slope <= 144) * 1;
-			} else {
-				return (slope <= 20) * 1 + 3;
-			}
-		}
+	uint16_t slope = (left ^ below) ? (dy * 100) / dx : (dx * 100) / dy; // Calculate the slope between points, flip x and y axis for top left or bottom right quadrants
 
-	} else {
-		if (target.y > start.y) {
-			uint8_t rise = target.y - start.y;
-			uint8_t run = start.x - target.x;
-			uint16_t slope = rise * 100 / run;
+	angle += (slope >= 20) + (slope >= 67) + (slope >= 150) + (slope >= 503); // Increment the angle if the slope is greater than certain values (instead of performing arctan approximation, check against pre-calculated values)
 
-			if (slope > 67) {
-				return (slope <= 503) * 1 + (slope <= 144) * 1 + 8;
-			} else {
-				return (slope <= 20) * 1 + 11;
-			}
-			
-		} else {
-			uint8_t rise = start.y - target.y;
-			uint8_t run = start.x - target.x;
-			uint16_t slope = rise * 100 / run;
-
-			if (slope > 67) {
-				return ((slope > 503) * 1 + (slope > 144) * 1 + 14) % 16;
-			} else {
-				return (slope > 20) * 1 + 12;
-			}
-		}
-	}
+	return angle;
 }
